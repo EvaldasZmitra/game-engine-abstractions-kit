f6c7a309b78caf4ca2821eb44a5fffb1a02f981f
evald <evaldas@inventi.lt>
2020-12-27 16:03:57 +0200

    Add prioritized actions

---
+++ gea-kit-tests/src/engine/PrioritySortedActions.cs
@@ -0,0 +1,40 @@
+using GeaKit.Engine;
+using Xunit;
+
+namespace GeaKit.Test {
+    public class EngineUpdateTest {
+        [Fact]
+        public void UpdatesAreExecuted() {
+            var engineUpdate = new PrioritySortedActions();
+            var executionCount = 0;
+            engineUpdate.AddUpdate(
+                (dt) => { executionCount++; }
+            );
+
+            engineUpdate.ExecuteAll(0);
+            engineUpdate.ExecuteAll(0);
+            Assert.Equal(1, executionCount);
+        }
+
+        [Fact]
+        public void UpdatesAreExecutedInOrder() {
+            var engineUpdate = new PrioritySortedActions();
+            var executionOrder = "";
+            engineUpdate.AddUpdate(
+                (dt) => { executionOrder += "1"; },
+                1
+            );
+            engineUpdate.AddUpdate(
+                (dt) => { executionOrder += "3"; },
+                3
+            );
+            engineUpdate.AddUpdate(
+                (dt) => { executionOrder += "2"; },
+                2
+            );
+
+            engineUpdate.ExecuteAll(0);
+            Assert.Equal("123", executionOrder);
+        }
+    }
+}
---
+++ gea-kit/src/engine/PrioritySortedActions.cs
@@ -0,0 +1,39 @@
+
+using System;
+using System.Collections.Generic;
+
+namespace GeaKit.Engine {
+    public class PrioritySortedActions {
+        private readonly SortedDictionary<int, List<Action<float>>> _actions = new();
+
+        public void AddUpdate(
+            Action<float> action,
+            int priority = 0
+        ) {
+            if (_actions.ContainsKey(priority)) {
+                _actions[priority].Add(action);
+            } else {
+                _actions.Add(
+                    priority,
+                    new List<Action<float>>() { action }
+                );
+            }
+        }
+
+        public void ExecuteAll(float deltaTime) {
+            foreach (var actionList in _actions) {
+                ExecuteActionList(actionList.Value, deltaTime);
+            }
+            _actions.Clear();
+        }
+
+        private static void ExecuteActionList(
+            List<Action<float>> actionList,
+            float deltaTime
+        ) {
+            foreach (var action in actionList) {
+                action.Invoke(deltaTime);
+            }
+        }
+    }
+}
